217d53f82821c897069270e047ee228a0a9fb68e
475738879 <475738879@qq.com>
2022-07-26 13:49:44 +0800

    初始的

---
+++ src/test/java/com/hxk/tesngDemo/TestDemo.java
@@ -0,0 +1,10 @@
+package com.hxk.tesngDemo;
+
+import org.testng.annotations.Test;
+
+public class TestDemo {
+    @Test(enabled = false)
+    public void test1(){
+        System.out.println("test1_1");
+    }
+}
---
+++ src/test/java/com/hxk/tesngDemo/TestDemo2.java
@@ -0,0 +1,10 @@
+package com.hxk.tesngDemo;
+
+import org.testng.annotations.Test;
+
+public class TestDemo2 {
+    @Test(enabled = false)
+    public void test2(){
+        System.out.println("test2_1");
+    }
+}
